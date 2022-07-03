be7fe8c6b1753fb37bbb8249270a9287be9efbd5
Joao Renato <joaorenatovaz@gmail.com>
2022-07-02 21:43:33 -0300

    adicionando a views render home

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(requets):
-    return HttpResponse('HOME')
+    return render(requets, 'recipes/home.html')
 
 def contato(request):
     return HttpResponse('CONTATO')
