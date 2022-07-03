6fbc9e99fd9ca27d206de28e55359e97055bd1d0
Joao Renato <joaorenatovaz@gmail.com>
2022-07-03 18:33:55 -0300

    deixando os conteudos das recipes dinamicos

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 def home(requets):
-    return render(requets, 'recipes/pages/home.html')
+    return render(requets, 'recipes/pages/home.html',context={'recipes':[make_recipe() for _ in range(10)],})
 
 def recipe(requets,id):
-    return render(requets, 'recipes/pages/recipe-view.html')
+    return render(requets, 'recipes/pages/recipe-view.html', context={'recipe':make_recipe(),})
 
