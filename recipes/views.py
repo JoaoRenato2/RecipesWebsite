10a16185b30c3b33cab7af8ce82a630407658742
Joao Renato <joaorenatovaz@gmail.com>
2022-07-03 18:45:45 -0300

    Adicionando o texto de preparo na page recipe

--- recipes/views.py
+++ recipes/views.py
@@ -6,5 +6,9 @@ def home(requets):
     return render(requets, 'recipes/pages/home.html',context={'recipes':[make_recipe() for _ in range(10)],})
 
 def recipe(requets,id):
-    return render(requets, 'recipes/pages/recipe-view.html', context={'recipe':make_recipe(),})
+    return render(requets, 'recipes/pages/recipe-view.html', context={
+    'recipe':make_recipe(),
+    'is_detail_page':True,
+    
+    })
 
